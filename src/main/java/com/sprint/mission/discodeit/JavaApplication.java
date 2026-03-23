6000646a909b3a87d88dc7231169a07aa08fa1ed
Woojin-Jeong <dnwls3958@naver.com>
2026-03-23 14:48:59 +0900

    test : add CRUD test for user, channel, message

--- src/main/java/com/sprint/mission/discodeit/JavaApplication.java
+++ src/main/java/com/sprint/mission/discodeit/JavaApplication.java
@@ -1,7 +1,13 @@
 package com.sprint.mission.discodeit;
 
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.Message;
 import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.service.ChannelService;
+import com.sprint.mission.discodeit.service.MessageService;
 import com.sprint.mission.discodeit.service.UserService;
+import com.sprint.mission.discodeit.service.jcf.JCFChannelService;
+import com.sprint.mission.discodeit.service.jcf.JCFMessageService;
 import com.sprint.mission.discodeit.service.jcf.JCFUserService;
 
 public class JavaApplication {
@@ -9,10 +15,18 @@ public class JavaApplication {
         // 사용자 테스트
         System.out.println("--------------- 사용자 테스트 시작! ---------------");
         UserService userService = new JCFUserService();
+        MessageService messageService = new JCFMessageService();
+        ChannelService channelService = new JCFChannelService();
 
-        User user = new User("test1","test1email.com","testnick","1234");
+        User user = new User("testUser1","testUser1@email.com","testNick","1234");
+        Channel channel = new Channel(user,"Notice");
+        Message message = new Message(channel,user,"testMessage","first Test message");
         userService.save(user);
+        channelService.save(channel);
+        messageService.save(message);
         System.out.println(userService.findAll());
+        System.out.println(channelService.findAll());
+        System.out.println(messageService.findAll());
         System.out.println("--------------- 사용자 테스트 종료! ---------------");
     }
 }
