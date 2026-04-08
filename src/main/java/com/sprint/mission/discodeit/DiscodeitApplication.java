34d94afb0fd88c228529eca3e1357b698441bece
apple <apple@appleui-MacBookAir.local>
2026-04-08 14:47:18 +0900

    refactor : 3차 미션 기본 요구사항 - Bean선언, Lombok적용

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -1,13 +1,46 @@
 package com.sprint.mission.discodeit;
 
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.ChannelType;
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.service.ChannelService;
+import com.sprint.mission.discodeit.service.MessageService;
+import com.sprint.mission.discodeit.service.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DiscodeitApplication {
+    static User setupUser(UserService userService) {
+        User user = userService.create("홍길덩", "dong@codeit.com", "dongdong");
+        return user;
+    }
+
+    static Channel setupChannel(ChannelService channelService) {
+        Channel channel = channelService.create(ChannelType.PUBLIC, "공지1", "공지 채널");
+        return channel;
+    }
+
+    static void messageCreateTest(MessageService messageService, Channel channel, User author) {
+        Message message = messageService.create("메시지1", channel.getId(), author.getId());
+        System.out.println("메시지 Id: " + message.getId() + ", 내용 : " + message.getContent() + ", 보낸 사람 : " + message.getAuthorId());
+    }
 
     public static void main(String[] args) {
-        SpringApplication.run(DiscodeitApplication.class, args);
+
+        ConfigurableApplicationContext context = SpringApplication.run(DiscodeitApplication.class, args);
+
+        UserService userService = context.getBean(UserService.class);
+        ChannelService channelService = context.getBean(ChannelService.class);
+        MessageService messageService = context.getBean(MessageService.class);
+
+        User user = setupUser(userService);
+        Channel channel = setupChannel(channelService);
+
+        messageCreateTest(messageService, channel, user);
+
     }
 
 }
