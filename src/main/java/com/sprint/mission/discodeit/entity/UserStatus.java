ee57b5a7846bdbeb5219e7d42d9005a9543f8c99
김태성 <taesung819@gmail.com>
2026-04-19 16:13:51 +0900

    feat: @Getter 어노테이션 추가

--- src/main/java/com/sprint/mission/discodeit/entity/UserStatus.java
+++ src/main/java/com/sprint/mission/discodeit/entity/UserStatus.java
@@ -1,10 +1,13 @@
 package com.sprint.mission.discodeit.entity;
 
+import lombok.Getter;
+
 import java.io.Serializable;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.UUID;
 
+@Getter
 public class UserStatus implements Serializable {
     private static final long serialVersionUID = 1L;
 
