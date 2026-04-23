7b633ee8eac1682b902498f30c9d4ab3c1ca305e
minzy108 <minzy108@gmail.com>
2026-04-23 17:45:33 +0900

    feat: 공개 채널 수정 및 채널 삭제 기능 구현

--- src/main/java/com/sprint/mission/discodeit/controller/ChannelController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/ChannelController.java
@@ -3,15 +3,15 @@ package com.sprint.mission.discodeit.controller;
 import com.sprint.mission.discodeit.dto.data.ChannelDto;
 import com.sprint.mission.discodeit.dto.request.PrivateChannelCreateRequest;
 import com.sprint.mission.discodeit.dto.request.PublicChannelCreateRequest;
+import com.sprint.mission.discodeit.dto.request.PublicChannelUpdateRequest;
 import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.service.ChannelService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/channels")
@@ -32,5 +32,21 @@ public class ChannelController {
         Channel channel = channelService.create(request);
         ChannelDto savedChannel = channelService.find(channel.getId());
         return ResponseEntity.status(HttpStatus.CREATED).body(savedChannel);
+    }
+
+    @RequestMapping(value = "/update/{id}", method = RequestMethod.PATCH)
+    public ResponseEntity<ChannelDto> updatePublicChannel(
+            @PathVariable UUID id,
+            @RequestBody PublicChannelUpdateRequest request) {
+        Channel channel = channelService.update(id, request);
+        ChannelDto updatedChannel = channelService.find(channel.getId());
+        return ResponseEntity.ok(updatedChannel);
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<ChannelDto> deleteChannel(@PathVariable UUID id) {
+        ChannelDto channel = channelService.find(id);
+        channelService.delete(id);
+        return ResponseEntity.status(HttpStatus.OK).body(channel);
     }
 }
