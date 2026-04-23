daf7059cf9c26461b2d44002b729c745150def2e
minzy108 <minzy108@gmail.com>
2026-04-23 17:02:57 +0900

    feat: 사용자 로그인 기능 추가

---
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -0,0 +1,24 @@
+package com.sprint.mission.discodeit.controller;
+
+import com.sprint.mission.discodeit.dto.data.UserDto;
+import com.sprint.mission.discodeit.dto.request.LoginRequest;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ResponseEntity<User> login(@RequestBody LoginRequest request) {
+        User loginUser = authService.login(request);
+        return ResponseEntity.ok(loginUser);
+    }
+
+}
