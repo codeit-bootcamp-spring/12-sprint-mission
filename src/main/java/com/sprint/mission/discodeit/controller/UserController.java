67835bc72f9b6d2d6fb6b2f9d9aea90e9c8f958a
minzy108 <minzy108@gmail.com>
2026-04-23 16:31:49 +0900

    feat: 사용자 온라인 상태 업데이트 기능 추가

--- src/main/java/com/sprint/mission/discodeit/controller/UserController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/UserController.java
@@ -3,14 +3,18 @@ package com.sprint.mission.discodeit.controller;
 import com.sprint.mission.discodeit.dto.data.UserDto;
 import com.sprint.mission.discodeit.dto.request.BinaryContentCreateRequest;
 import com.sprint.mission.discodeit.dto.request.UserCreateRequest;
+import com.sprint.mission.discodeit.dto.request.UserStatusUpdateRequest;
 import com.sprint.mission.discodeit.dto.request.UserUpdateRequest;
 import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.entity.UserStatus;
 import com.sprint.mission.discodeit.service.UserService;
+import com.sprint.mission.discodeit.service.UserStatusService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -20,6 +24,7 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final UserStatusService userStatusService;
 
     @RequestMapping(value = "/create", method = RequestMethod.POST)
     public ResponseEntity<UserDto> createUser (
@@ -51,5 +56,16 @@ public class UserController {
     public ResponseEntity<List<UserDto>> findAll() {
         List<UserDto> userList = userService.findAll();
         return ResponseEntity.ok(userList);
-        }
+    }
+
+    @RequestMapping(value = "/{id}/status", method = RequestMethod.PUT)
+    public ResponseEntity<UserStatus> updateUserStatus(@PathVariable UUID id) {
+        UserStatus updatedStatus = userStatusService.updateByUserId(
+                id,
+                new UserStatusUpdateRequest(Instant.now())
+        );
+        return ResponseEntity.ok(updatedStatus);
+    }
+
+
 }
