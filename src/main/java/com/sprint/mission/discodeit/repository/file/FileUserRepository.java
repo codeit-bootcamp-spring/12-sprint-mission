31564b3cc5d214ea0cefc5449c296e378a5b20a3
gusdn6763 <gusdn6763@gmail.com>
2026-04-27 14:12:05 +0900

    refactor: 레포지토리 구조 개선
    1. 폴더가 없으면 테스트 불가능한 문제 수정

--- src/main/java/com/sprint/mission/discodeit/repository/file/FileUserRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/file/FileUserRepository.java
@@ -2,19 +2,12 @@ package com.sprint.mission.discodeit.repository.file;
 
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.repository.UserRepository;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.nio.file.Files;
+import com.sprint.mission.discodeit.util.FileSerialization;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.stream.Stream;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Repository;
@@ -23,68 +16,30 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class FileUserRepository implements UserRepository {
 
-  private final Path DIRECTORY;
-  private final String EXTENSION = ".ser";
+  private final Path directory;
   private final FileLockProvider fileLockProvider;
 
   public FileUserRepository(
       @Value("${discodeit.repository.file-directory:data}") String fileDirectory,
       FileLockProvider fileLockProvider
   ) {
-    this.DIRECTORY = Paths.get(System.getProperty("user.dir"), fileDirectory,
+    this.directory = Paths.get(System.getProperty("user.dir"), fileDirectory,
         User.class.getSimpleName());
-    if (Files.notExists(DIRECTORY)) {
-      try {
-        Files.createDirectories(DIRECTORY);
-      } catch (IOException e) {
-        throw new RuntimeException(e);
-      }
-    }
     this.fileLockProvider = fileLockProvider;
   }
 
   private Path resolvePath(UUID id) {
-    return DIRECTORY.resolve(id + EXTENSION);
+    return FileSerialization.resolvePath(directory, id);
   }
 
   @Override
   public User save(User user) {
-    Path path = resolvePath(user.getId());
-    ReentrantLock lock = fileLockProvider.getLock(path);
-    lock.lock();
-
-    try (
-        FileOutputStream fos = new FileOutputStream(path.toFile());
-        ObjectOutputStream oos = new ObjectOutputStream(fos)
-    ) {
-      oos.writeObject(user);
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    } finally {
-      lock.unlock();
-    }
-    return user;
+    return FileSerialization.save(directory, resolvePath(user.getId()), user, fileLockProvider);
   }
 
   @Override
   public Optional<User> findById(UUID id) {
-    User userNullable = null;
-    Path path = resolvePath(id);
-    ReentrantLock lock = fileLockProvider.getLock(path);
-    lock.lock();
-    if (Files.exists(path)) {
-      try (
-          FileInputStream fis = new FileInputStream(path.toFile());
-          ObjectInputStream ois = new ObjectInputStream(fis)
-      ) {
-        userNullable = (User) ois.readObject();
-      } catch (IOException | ClassNotFoundException e) {
-        throw new RuntimeException(e);
-      } finally {
-        lock.unlock();
-      }
-    }
-    return Optional.ofNullable(userNullable);
+    return FileSerialization.findById(directory, resolvePath(id), fileLockProvider, User.class);
   }
 
   @Override
@@ -96,43 +51,17 @@ public class FileUserRepository implements UserRepository {
 
   @Override
   public List<User> findAll() {
-    try (Stream<Path> paths = Files.list(DIRECTORY)) {
-      return paths
-          .filter(path -> path.toString().endsWith(EXTENSION))
-          .map(path -> {
-            ReentrantLock lock = fileLockProvider.getLock(path);
-            lock.lock();
-            try (
-                FileInputStream fis = new FileInputStream(path.toFile());
-                ObjectInputStream ois = new ObjectInputStream(fis)
-            ) {
-              return (User) ois.readObject();
-            } catch (IOException | ClassNotFoundException e) {
-              throw new RuntimeException(e);
-            }  finally {
-              lock.unlock();
-            }
-          })
-          .toList();
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
+    return FileSerialization.findAll(directory, fileLockProvider, User.class);
   }
 
   @Override
   public boolean existsById(UUID id) {
-    Path path = resolvePath(id);
-    return Files.exists(path);
+    return FileSerialization.exists(directory, resolvePath(id));
   }
 
   @Override
   public void deleteById(UUID id) {
-    Path path = resolvePath(id);
-    try {
-      Files.delete(path);
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
+    FileSerialization.delete(directory, resolvePath(id), fileLockProvider);
   }
 
   @Override
