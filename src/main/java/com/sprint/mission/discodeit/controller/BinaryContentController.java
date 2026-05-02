abea65feb5f891e3408237d65dc944057090d2e9
minzy108 <minzy108@gmail.com>
2026-05-02 19:10:01 +0900

    refactor: refactor BinaryContentController to RESTful API

--- src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
@@ -5,22 +5,20 @@ import com.sprint.mission.discodeit.service.BinaryContentService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
 
-@Controller
-@ResponseBody
-@RequestMapping("/api/binaryContent")
+@RestController
+@RequestMapping("/api/binaryContents")
 @AllArgsConstructor
 public class BinaryContentController {
 
   private final BinaryContentService binaryContentService;
 
-  @RequestMapping(path = "find")
-  public ResponseEntity<BinaryContent> find(@RequestParam("binaryContentId") UUID binaryContentId) {
+  @GetMapping("/{binaryContentId}")
+  public ResponseEntity<BinaryContent> find(@PathVariable UUID binaryContentId) {
     BinaryContent binaryContent =
         binaryContentService.find(binaryContentId);
     return ResponseEntity
@@ -28,10 +26,9 @@ public class BinaryContentController {
         .body(binaryContent);
   }
 
-  @RequestMapping(path = "findAllByIdIn")
-  public ResponseEntity<List<BinaryContent>> findAllByIdIn
-      (@RequestParam("binaryContentIds") List<UUID> binaryContentIds
-      ) {
+  @GetMapping
+  public ResponseEntity<List<BinaryContent>> findAllByIdIn(
+      @RequestParam List<UUID> binaryContentIds) {
     List<BinaryContent> binaryContents = binaryContentService.findAllByIdIn(binaryContentIds);
     return ResponseEntity
         .status(HttpStatus.OK)
