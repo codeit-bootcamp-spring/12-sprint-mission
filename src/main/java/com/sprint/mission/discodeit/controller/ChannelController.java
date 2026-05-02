131ddabcdf9c13860d2343a91f4eba0aafa70b7d
seongjuun <beajon0301@naver.com>
2026-05-02 22:26:15 +0900

    feat: Swagger API 문서 및 관련 인터페이스 추가

--- src/main/java/com/sprint/mission/discodeit/controller/ChannelController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/ChannelController.java
@@ -1,10 +1,16 @@
 package com.sprint.mission.discodeit.controller;
 
+import com.sprint.mission.discodeit.controller.api.ChannelApi;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,35 +28,41 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/channel")
-public class ChannelController {
-	private final ChannelService channelService;
+@RequestMapping("/api/channels")
+public class ChannelController implements ChannelApi {
 
-	@RequestMapping(path = "/public", method = RequestMethod.POST)
-	public ResponseEntity<Channel> create(@RequestBody PublicChannelCreateRequest request) {
-		return ResponseEntity.ok(channelService.create(request));
-	}
+  private final ChannelService channelService;
 
-	@RequestMapping(path = "/private", method = RequestMethod.POST)
-	public ResponseEntity<Channel> create(@RequestBody PrivateChannelCreateRequest request) {
-		return ResponseEntity.ok(channelService.create(request));
-	}
+  @PostMapping("/public")
+  @Override
+  public ResponseEntity<Channel> createPublic(@RequestBody PublicChannelCreateRequest request) {
+    return ResponseEntity.status(HttpStatus.CREATED).body(channelService.create(request));
+  }
 
-	@RequestMapping(path = "/public/{channelId}", method = RequestMethod.PATCH)
-	public ResponseEntity<Channel> update(
-		@PathVariable UUID channelId,
-		@RequestBody PublicChannelUpdateRequest request){
-		return ResponseEntity.ok(channelService.update(channelId, request));
-	}
+  @PostMapping("/private")
+  @Override
+  public ResponseEntity<Channel> createPrivate(@RequestBody PrivateChannelCreateRequest request) {
+    return ResponseEntity.status(HttpStatus.CREATED).body(channelService.create(request));
+  }
 
-	@RequestMapping(path = "/{channelId}", method = RequestMethod.DELETE)
-	public ResponseEntity<Void> delete(@PathVariable UUID channelId){
-		channelService.delete(channelId);
-		return ResponseEntity.noContent().build();
-	}
+  @PatchMapping("/{channelId}")
+  @Override
+  public ResponseEntity<Channel> update(
+      @PathVariable UUID channelId,
+      @RequestBody PublicChannelUpdateRequest request) {
+    return ResponseEntity.ok(channelService.update(channelId, request));
+  }
 
-	@RequestMapping(path = "/", method = RequestMethod.GET)
-	public ResponseEntity<List<ChannelDto>> find(@RequestParam UUID userId) {
-		return ResponseEntity.ok(channelService.findAllByUserId(userId));
-	}
+  @DeleteMapping("/{channelId}")
+  @Override
+  public ResponseEntity<Void> delete(@PathVariable UUID channelId) {
+    channelService.delete(channelId);
+    return ResponseEntity.noContent().build();
+  }
+
+  @GetMapping()
+  @Override
+  public ResponseEntity<List<ChannelDto>> find(@RequestParam(value = "userId") UUID userId) {
+    return ResponseEntity.ok(channelService.findAllByUserId(userId));
+  }
 }
--- src/main/java/com/sprint/mission/discodeit/controller/ReadStatusController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/ReadStatusController.java
@@ -1,10 +1,15 @@
 package com.sprint.mission.discodeit.controller;
 
+import com.sprint.mission.discodeit.controller.api.ReadStatusApi;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,24 +25,28 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/readStatus")
-public class ReadStatusController {
-	private final ReadStatusService readStatusService;
+@RequestMapping("/api/readStatuses")
+public class ReadStatusController implements ReadStatusApi {
 
-	@RequestMapping(path = "/", method = RequestMethod.POST)
-	public ResponseEntity<ReadStatus> create(@RequestBody ReadStatusCreateRequest request){
-		return ResponseEntity.ok(readStatusService.create(request));
-	}
+  private final ReadStatusService readStatusService;
 
-	@RequestMapping(path = "/{readStatusId}", method = RequestMethod.PATCH)
-	public ResponseEntity<ReadStatus> update(
-		@PathVariable UUID readStatusId,
-		@RequestBody ReadStatusUpdateRequest request){
-		return ResponseEntity.ok(readStatusService.update(readStatusId, request));
-	}
+  @PostMapping()
+  @Override
+  public ResponseEntity<ReadStatus> create(@RequestBody ReadStatusCreateRequest request) {
+    return ResponseEntity.status(HttpStatus.CREATED).body(readStatusService.create(request));
+  }
 
-	@RequestMapping(path = "/", method = RequestMethod.GET)
-	public ResponseEntity<List<ReadStatus>> findAll(@RequestParam(value = "userId") UUID userId){
-		return ResponseEntity.ok(readStatusService.findAllByUserId(userId));
-	}
+  @PatchMapping("/{readStatusId}")
+  @Override
+  public ResponseEntity<ReadStatus> update(
+      @PathVariable UUID readStatusId,
+      @RequestBody ReadStatusUpdateRequest request) {
+    return ResponseEntity.ok(readStatusService.update(readStatusId, request));
+  }
+
+  @GetMapping()
+  @Override
+  public ResponseEntity<List<ReadStatus>> findAll(@RequestParam(value = "userId") UUID userId) {
+    return ResponseEntity.ok(readStatusService.findAllByUserId(userId));
+  }
 }
