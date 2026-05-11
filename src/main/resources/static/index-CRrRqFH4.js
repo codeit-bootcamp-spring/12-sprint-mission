465018b214679551b57c7e2a0642d39c5ff4dc03
Seungji Lyoo <lyoonatelie@gmail.com>
2026-05-11 17:39:35 +0900

    f9

--- src/main/resources/static/index-CRrRqFH4.js
+++ src/main/resources/static/index-CRrRqFH4.js
@@ -842,7 +842,7 @@ Error generating stack: `+a.message+`
       const x = {url, contentType, originalName, size: blob.size};
       return r(E => ({attachments: {...E.attachments, [s]: x}}))
     } catch (u) {
-      return console.error("첨부파일 로딩 실패:", u), null
+      return console.error("첨부파일 로딩 실패:", u), null;
     }
   };
   const h1=r=>r<1024?r+" B":r<1024*1024?(r/1024).toFixed(2)+" KB":r<1024*1024*1024?(r/(1024*1024)).toFixed(2)+" MB":(r/(1024*1024*1024)).toFixed(2)+" GB";function m1({channel:r}){const i=vo(P=>P.messages),s=vo(P=>P.fetchMessages),u=vo(P=>P.startPolling),c=vo(P=>P.stopPolling),d=Wt(P=>P.profileImages),p=Dt(P=>P.users),{attachments:m,fetchAttachment:v}=p1();ue.useEffect(() => {
