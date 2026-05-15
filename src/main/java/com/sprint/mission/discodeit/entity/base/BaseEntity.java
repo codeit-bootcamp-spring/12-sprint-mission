d067e5f25d108f4db5c1d53dce8e58a7153bf851
hyeon1226 <rygus0128@gmail.com>
2026-05-15 21:31:15 +0900

    feat: 공통 엔티티 기반 클래스 추가

---
+++ src/main/java/com/sprint/mission/discodeit/entity/base/BaseEntity.java
@@ -0,0 +1,23 @@
+package com.sprint.mission.discodeit.entity.base;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @Id
+    private UUID id;
+
+    @CreatedDate
+    private Instant createdAt;
+}
