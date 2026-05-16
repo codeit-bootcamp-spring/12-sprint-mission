cffa815fd180599089a68175e71280f30c748f9f
minzy108 <minzy108@gmail.com>
2026-05-16 21:07:58 +0900

    feat: feat: PostgreSQL 및 JPA 환경 설정

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,110 @@
+CREATE USER discodeit_user WITH PASSWORD 'discodeit1234';
+
+CREATE DATABASE discodeit
+    OWNER discodeit_user;
+
+GRANT ALL PRIVILEGES ON DATABASE discodeit TO discodeit_user;
+
+GRANT ALL ON SCHEMA public TO discodeit_user;
+
+CREATE TABLE binary_contents
+(
+    id           uuid PRIMARY KEY,
+    created_at   timestamptz  NOT NULL,
+    file_name    VARCHAR(255) NOT NULL,
+    size         BIGINT       NOT NULL,
+    content_type VARCHAR(100) NOT NULL,
+    bytes        bytea        NOT NULL
+);
+
+CREATE TABLE users
+(
+    id         uuid PRIMARY KEY,
+    created_at timestamptz,
+    updated_at timestamptz,
+    username   VARCHAR(50)  NOT NULL UNIQUE,
+    email      VARCHAR(100) NOT NULL UNIQUE,
+    password   VARCHAR(60)  NOT NULL,
+    profile_id uuid,
+    CONSTRAINT fk_users_profile
+        FOREIGN KEY (profile_id)
+            REFERENCES binary_contents (id)
+            ON DELETE SET NULL
+);
+
+CREATE TABLE channels
+(
+    id          uuid PRIMARY KEY,
+    created_at  timestamptz NOT NULL,
+    updated_at  timestamptz,
+    name        VARCHAR(100),
+    description VARCHAR(500),
+    type        VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE messages
+(
+    id         uuid PRIMARY KEY,
+    created_at timestamptz NOT NULL,
+    updated_at timestamptz,
+    content    TEXT,
+    channel_id uuid        NOT NULL,
+    author_id  uuid,
+    CONSTRAINT fk_messages_channel
+        FOREIGN KEY (channel_id)
+            REFERENCES channels (id)
+            ON DELETE CASCADE,
+    CONSTRAINT fk_messages_author
+        FOREIGN KEY (author_id)
+            REFERENCES users (id)
+            ON DELETE SET NULL
+);
+
+CREATE TABLE message_attachments
+(
+    message_id    uuid NOT NULL,
+    attachment_id uuid NOT NULL,
+    PRIMARY KEY (message_id, attachment_id),
+    CONSTRAINT fk_message_attachments_message
+        FOREIGN KEY (message_id)
+            REFERENCES messages (id)
+            ON DELETE CASCADE,
+    CONSTRAINT fk_message_attachments_attachment
+        FOREIGN KEY (attachment_id)
+            REFERENCES binary_contents (id)
+            ON DELETE CASCADE
+);
+
+CREATE TABLE user_statuses
+(
+    id             uuid PRIMARY KEY,
+    created_at     timestamptz NOT NULL,
+    updated_at     timestamptz,
+    user_id        uuid        NOT NULL UNIQUE,
+    last_active_at timestamptz NOT NULL,
+    CONSTRAINT fk_user_statuses_user
+        FOREIGN KEY (user_id)
+            REFERENCES users (id)
+            ON DELETE CASCADE
+);
+
+CREATE TABLE read_statuses
+(
+    id           uuid PRIMARY KEY,
+    created_at   timestamptz NOT NULL,
+    updated_at   timestamptz,
+    user_id      uuid        NOT NULL,
+    channel_id   uuid        NOT NULL,
+    last_read_at timestamptz NOT NULL,
+    CONSTRAINT fk_read_statuses_user
+        FOREIGN KEY (user_id)
+            REFERENCES users (id)
+            ON DELETE CASCADE,
+    CONSTRAINT fk_read_statuses_channel
+        FOREIGN KEY (channel_id)
+            REFERENCES channels (id)
+            ON DELETE CASCADE,
+    CONSTRAINT uk_read_statuses_user_channel
+        UNIQUE (user_id, channel_id)
+);
+
