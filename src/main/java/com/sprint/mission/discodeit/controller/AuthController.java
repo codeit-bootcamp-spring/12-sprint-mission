5d42a503731d2b1f36b044d80937f89c76e6af18
ozingeodari <ozingeodari@gmail.com>
2026-07-28 17:07:14 +0900

    chore: sprint9 베이스 코드로 초기화

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -1,14 +1,20 @@
 package com.sprint.mission.discodeit.controller;
 
 import com.sprint.mission.discodeit.controller.api.AuthApi;
+import com.sprint.mission.discodeit.dto.data.UserDto;
 import com.sprint.mission.discodeit.dto.request.LoginRequest;
-import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.service.AuthService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/auth")
@@ -16,9 +22,11 @@ public class AuthController implements AuthApi {
 
   private final AuthService authService;
 
-  @PostMapping(path = "/login")
-  public ResponseEntity<User> login(@RequestBody LoginRequest loginRequest) {
-    User user = authService.login(loginRequest);
+  @PostMapping(path = "login")
+  public ResponseEntity<UserDto> login(@RequestBody @Valid LoginRequest loginRequest) {
+    log.info("로그인 요청: username={}", loginRequest.username());
+    UserDto user = authService.login(loginRequest);
+    log.debug("로그인 응답: {}", user);
     return ResponseEntity
         .status(HttpStatus.OK)
         .body(user);
